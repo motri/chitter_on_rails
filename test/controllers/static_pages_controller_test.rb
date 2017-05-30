203c407c39e6271f0360ea0123d6cfe21e71ecfe
Unai Motriko <unaimotriko@gmail.com>
2017-05-30 13:48:22 +0100

    it tests the root url

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,10 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
     def setup
       @base_title = "Ruby on Rails Tutorial Sample App"
     end
-    
+
     test "should get home" do
       get static_pages_home_url
       assert_response :success
