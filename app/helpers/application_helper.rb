117f661c705a0542116e4bddd4c771d89242c123
Unai Motriko <unaimotriko@gmail.com>
2017-05-31 18:14:08 +0100

    i

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,7 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-<<<<<<< HEAD
-<<<<<<< HEAD
       page_title + " | " + page_title
-=======
-      page_title + " | " + base_title
->>>>>>> 850ff148aacb9849c8e0c274ea9ca179e5d2acf9
-=======
-      page_title + " | " + base_title
->>>>>>> 944b4b6318892960c2913b7743b45d42c35cdb4b
     end
   end
 end
