921d09abab7cefa652792e455c86366c235466b5
Unai Motriko <unaimotriko@gmail.com>
2017-06-17 21:10:57 +0100

    adds travis config

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,20 @@ class UserTest < ActiveSupport::TestCase
       @user.name = " "
       assert_not @user.valid?
     end
+
+    test "email should be present" do
+      @user.email = " "
+      assert_not @user.valid?
+    end
+
+    test "name should not be too long" do
+      @user.name = "a" * 51
+      assert_not @user.valid?
+    end
+
+    test "email should not be too long" do
+      @user.email = "a" * 200 + "@mail.com"
+      assert_not @user.valid?
+    end
+
 end
